b3f0ab5a759120dec72807b1649095110c4fa51d
naveen kumar <naveen.lingampelli21@gmail.com>
2025-06-13 19:06:16 +0530

    roboshop updated

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,7 +8,7 @@ check_root
 echo "please enter rabbitmq password to setup"
 read -s RABBITMQ_PASSWD
 
-cp Rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
 VALIDATE $? "Adding rabbitmq repo"
 
 dnf install rabbitmq-server -y &>>$LOG_FILE
